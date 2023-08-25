420218d0c2f611ed40c8c0ff4ebacff260590282
Biplob Sutradhar <biplobsd11@gmail.com>
2023-08-25 22:35:58 +0600

    Formatted

--- packages/app/features/Search/screen.tsx
+++ packages/app/features/Search/screen.tsx
@@ -1,25 +1,15 @@
-import React from 'react'; // Make sure to import React
-import { Menu, Search, UserCircle } from '@tamagui/lucide-icons';
-import { Button } from 'tamagui';
-import { XStack,YStack,Input,TextArea } from 'tamagui';
-import { red } from '@tamagui/themes';
+import React from 'react'
+import { Search } from '@tamagui/lucide-icons'
+import { Button } from 'tamagui'
+import { XStack, YStack, Input } from 'tamagui'
 
 export const SearchBar = () => (
-    <YStack>
-
-    
-    <XStack padding={10} justifyContent='center'>
-    <Input size="$3.5" borderWidth={2} borderRadius={22}  width={440} marginRight={9}/>
-    <Button icon={Search} borderRadius={22}>Search</Button>
-    
+  <YStack>
+    <XStack padding={10} justifyContent="center">
+      <Input size="$3.5" borderWidth={2} borderRadius={22} width={440} marginRight={9} />
+      <Button icon={Search} borderRadius={22}>
+        Search
+      </Button>
     </XStack>
-    
-
-
-    </YStack>
-  )
-
-
-
-  
-
+  </YStack>
+)
