f2f24cf6e92cf0b1bb9dc009496e8054a913eeb7
Biplob Sutradhar <biplobsd11@gmail.com>
2023-08-25 22:33:07 +0600

    Back to default

--- apps/expo/app/index.tsx
+++ apps/expo/app/index.tsx
@@ -1,18 +1,15 @@
-import { XStack } from '@my/ui/types'
 import { HomeScreen } from 'app/features/home/screen'
-import { SearchBar } from 'app/features/Search/screen'
 import { Stack } from 'expo-router'
 
 export default function Screen() {
   return (
-    <XStack>
+    <>
       <Stack.Screen
         options={{
-          title: 'Search',
+          title: 'Home',
         }}
       />
-      {/* <HomeScreen /> */}
-      <SearchBar />
-    </XStack>
+      <HomeScreen />
+    </>
   )
 }
